d2035418e5b857e31b9de5b6280eadff696c054a
gitAV <akhandverma01062003@gmail.com>
2023-06-21 07:40:18 +0530

    Final Commit

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-var items=["Buy Food","Cook Food","Eat Food"];
+let workItems = [];
+let items = ["Buy Food", "Cook Food", "Eat Food"];
 app.get("/", function (req, res) {
-    var today = new Date();
-    var options={
-        weekday: "long",
-        day: "numeric",
-        month: "long"
+    let day = date.getDate();
+    res.render("list", { ListTitle: day, newListItems: items });
+});
+app.post("/", function (req, res) {
+    if (req.body.list === "Work") {
+        workItems.push(req.body.newItem);
+        res.redirect("/work");
+    } else {
+        items.push(req.body.newItem);
+        res.redirect("/");
     }
-    var day=today.toLocaleDateString("en-US",options);
-    res.render("list",{kindOfDay: day,newListItems: items});
 });
-app.post("/",function(req,res){
-    items.push(req.body.newItem);
-    res.redirect("/");
+app.get("/work", function (req, res) {
+    res.render("list", { ListTitle: "Work List", newListItems: workItems });
 });
-
 app.listen(3000, function () {
     console.log("Server started on port 3000.");
 });
